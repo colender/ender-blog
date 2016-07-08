144b5d17e4813c509d0efc3abcbf8314f4c00362
Ruben Rangel <colender.m@gmail.com>
2016-07-08 16:48:49 -0700

    First Push

---
+++ database/migrations/2016_07_08_195008_createArticleTable.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('posts',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('title')->index();
+            $table->string('content');
+            $table->string('slug')->index();
+            $table->integer('created_by')->unsigned();
+            $table->foreign('created_by')->references('id')->on('users');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('posts');
+    }
+}
